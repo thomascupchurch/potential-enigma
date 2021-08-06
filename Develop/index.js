b97ba79a0747e23396878324c8290762a393f48a
Boba Fett <thomascupchurch@gmail.com>
2021-08-06 13:40:42 -0500

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,105 @@
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMD = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const promptReadme = () => {
+    console.log(`
+  =================
+  Make a New README
+  =================
+  `);
+    
+    return inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is the title of your website?" (Required)',
+          validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project name!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a description of your website (Required)',
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('You need to enter a website description!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What languages did you use? (Check all that apply)',
+          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+       
+        {
+          type: 'confirm',
+          name: 'badges',
+          message: 'Would you like to add badges to your README?',
+          default: false
+        },
+
+        {
+          type: 'checkbox',
+          name: 'badges',
+          message: 'What badges do you want? (Check all that apply)',
+          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+      ])
+      .then((inquirerData) => {
+        const input = generateMD(inquirerData);
+        writeToFile(input);
+      }
+      )
+  };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+promptReadme();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,38 @@
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // if statement
+  if (license) () => {
+
+  }
+  // take in an array of badges specified by user
+
+  // return string of selected badges
+  // call this inside generateMarkdown
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +44,13 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const chosenBadges = renderLicenseBadge(data.badges);
   return `# ${data.title}
+  ## ${chosenLicense}
+  
 
 `;
+
 }
 
 module.exports = generateMarkdown;
